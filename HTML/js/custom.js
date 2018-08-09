3c4e67914c3993a6972d51e38b5e2ffb2efc3d62
Upesh Vishwakarma <100517@mediserve.com>
2018-08-09 21:06:16 +0530

    Created Tasty pancakes section with two custom slider

--- HTML/js/custom.js
+++ HTML/js/custom.js
@@ -9,5 +9,48 @@
       else sticky.removeClass('top');
     });
 
+    $('.smallcakes_slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    $('.pancakes_inner_block').slick({
+      dots: true,
+      arrows:false,
+      vertical: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+
   });
 })(jQuery);
