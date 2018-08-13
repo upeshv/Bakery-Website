8d21507b97ca6692259155c603b3b3f6b76b116c
Upesh Vishwakarma <100517@mediserve.com>
2018-08-13 20:53:16 +0530

    Bakery completed HTML with repsonsive

--- HTML/js/custom.js
+++ HTML/js/custom.js
@@ -11,7 +11,7 @@
 
     $('.smallcakes_slider').slick({
       dots: false,
-      infinite: true,
+      arrows:true,
       speed: 300,
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -26,7 +26,7 @@
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 768,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 1
@@ -35,7 +35,7 @@
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 2,
             slidesToScroll: 1
           }
         }
@@ -52,5 +52,18 @@
       adaptiveHeight: true
     });
 
+    var accordionClone = $('.navigation_bar').clone();
+    $( ".mobile_nav" ).append(accordionClone);
+
+    $('#header_toggle').click(function() {
+      $(this).toggleClass('open', 1000);
+      $('.navigation_bar').toggleClass('open');
+      $('body').toggleClass('open', 1000);
+    });   
+
+    var val1 = parseInt($('.ready_time .val1').text());
+    var val2 = parseInt($('.ready_time .val2').text());
+    var val3 = val1+val2;
+    $('.ready_time .val3').text(val3);
   });
 })(jQuery);
